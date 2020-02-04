a582779cdcc8804f8eac1ed086f64781cce9be10
Eze Sunday Eze <mailsforeze@gmail.com>
2020-02-04 10:30:58 +0100

    refactor: delete unnecesary files

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from 'react'
 import './App.css'
 import Home from './Components/Home'
 import { Route, Switch } from 'react-router-dom'
-import DeckContainer from './Components/DeckContainer'
-import Login from './Components/Login'
 import NotFound from './Components/404/404'
 
 function App () {
@@ -12,8 +10,6 @@ function App () {
     <div className='App'>
       <Switch>
         <Route exact path='/' component={Home} />
-        <Route path='/login/:id' component={Login} />
-        <Route path='/campaign/:id' component={DeckContainer} />
         <Route path='*' component={NotFound} />
       </Switch>
     </div>
