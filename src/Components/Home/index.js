d1ad2e031c0f1daf20dd54677c77414dd15b0c29
Eze Sunday Eze <mailsforeze@gmail.com>
2020-02-05 12:30:39 +0100

    fix: removed unnecessary data

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -8,9 +8,7 @@ import './index.css'
    <React.Fragment>
     <div class="container">
         <div class="avatar-flip">
-        <img alt='' src="https://pbs.twimg.com/profile_images/1195821518267330561/-JH3S3aT_400x400.jpg" height="150" width="150" />
-        <img alt='' src="http://i1112.photobucket.com/albums/k497/animalsbeingdicks/abd-3-12-2015.gif~original" height="150" width="150" />
-        
+        <img alt='eze' src="https://pbs.twimg.com/profile_images/1195821518267330561/-JH3S3aT_400x400.jpg" height="150" width="150" />
         </div>
             <h2>Eze Sunday Eze</h2>
             <h4>Software Developer</h4>
